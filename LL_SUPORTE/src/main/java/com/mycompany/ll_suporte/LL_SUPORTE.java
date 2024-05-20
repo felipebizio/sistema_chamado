4e9993a99f211be90c951ea3a9fe806b74d190f8
felipebizio <felipexavierluizbizio@gmail.com>
2024-05-20 19:02:28 -0300

    Criação do projeto
    1° commit do projeto

---
+++ LL_SUPORTE/src/main/java/com/mycompany/ll_suporte/LL_SUPORTE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ll_suporte;
+
+/**
+ *
+ * @author lbizio
+ */
+public class LL_SUPORTE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
